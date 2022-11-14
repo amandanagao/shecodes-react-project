6192ca144124863e0b5b826048181edb04b5e211
amandanagao <nagaoamanda@gmail.com>
2022-11-14 10:52:41 -0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ import "./App.css";
 export default function App() {
     return (
         <div className="App">
-            <Card className="card">
-                <Card.Header className="header">
-                    Wed, 09/11/2022 - 10:12
-                </Card.Header>
-                <WeatherSearch />
-                <Forecast />
-                <Units />
-            </Card>
-            <div className="coder">
-                <a
-                    href="https://github.com/amandanagao/shecodes-react-project"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    Open-source code
-                </a>{" "}
-                by Amanda Nagao
+            <div className="container">
+                <Card className="card">
+                    <Card.Header className="header">
+                        Wed, 09/11/2022 - 10:12
+                    </Card.Header>
+                    <WeatherSearch />
+                    <Forecast />
+                    <Units />
+                </Card>
+                <div className="coder">
+                    <a
+                        href="https://github.com/amandanagao/shecodes-react-project"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Open-source code
+                    </a>{" "}
+                    by Amanda Nagao
+                </div>
             </div>
         </div>
     );
