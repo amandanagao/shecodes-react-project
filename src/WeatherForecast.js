478bd2e3bb98b3c85ea231fdb2767dd603c7c3ea
amandanagao <nagaoamanda@gmail.com>
2022-11-16 14:16:58 -0300

    Added WeatherForecastDay.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,41 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-import "./WeatherForecast.css";
-
 export default function WeatherForecast(props) {
-    function displayWeatherForecast(response) {}
+    const [loaded, setLoaded] = useState(false);
+    const [forecast, setForecast] = useState(null);
 
-    let latitude = props.coordinates.latitude;
-    let longitude = props.coordinates.longitude;
-    let apiKey = "b220773ot9b8ef196b845b21b5cabb26";
-    let unit = "metric";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=${unit}`;
-    axios.get(apiUrl).then(displayWeatherForecast);
+    function displayWeatherForecast(response) {
+        console.log(response.data);
+        setLoaded(true);
+        setForecast(response.data.daily);
+    }
 
-    return (
-        <div className="WeatherForecast">
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="WeatherForecast-day">Thu</div>
-                        <WeatherIcon code={"clear-sky-day"} size={36} />
-                        <div className="WeatherForecast-temperature">
-                            <span className="WeatherForecast-temperature-max">
-                                29°
-                            </span>
-                            <span className="WeatherForecast-temperature-min">
-                                20°
-                            </span>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+    if (loaded) {
+        console.log(forecast);
+        return (
+            <div className="WeatherForecast">
+                <Container>
+                    <Row>
+                        <Col>
+                            <WeatherForecastDay data={forecast[1]} />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    } else {
+        let latitude = props.coordinates.latitude;
+        let longitude = props.coordinates.longitude;
+        let apiKey = "b220773ot9b8ef196b845b21b5cabb26";
+        let unit = "metric";
+        let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=${unit}`;
+        axios.get(apiUrl).then(displayWeatherForecast);
+
+        return null;
+    }
 }
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,38 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+import "./WeatherForecastDay.css";
+
+export default function WeatherForecastDay(props) {
+    function day() {
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        let date = new Date(props.data.time * 1000);
+        let day = date.getDay();
+        return days[day];
+    }
+
+    function maxTemperature() {
+        let temperature = Math.round(props.data.temperature.maximum);
+        return `${temperature}°`;
+    }
+
+    function minTemperature() {
+        let temperature = Math.round(props.data.temperature.minimum);
+        return `${temperature}°`;
+    }
+
+    return (
+        <div className="WeatherForecastDay">
+            <div className="WeatherForecastDay-day">{day()}</div>
+            <WeatherIcon code={props.data.condition.icon} size={36} />
+            <div className="WeatherForecastDay-temperature">
+                <span className="WeatherForecastDay-temperature-max">
+                    {maxTemperature()}
+                </span>
+                <span className="WeatherForecastDay-temperature-min">
+                    {minTemperature()}
+                </span>
+            </div>
+        </div>
+    );
+}
